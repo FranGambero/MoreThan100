1f9cdb77e179cbdb0ba629a0f8d7bee2f859dde5
Fran Gambero <frangansal.13@gmail.com>
2024-06-25 20:13:46 +0200

    added jobRole to offer SO

--- Assets/Scripts/Offer.cs
+++ Assets/Scripts/Offer.cs
@@ -6,8 +6,13 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "New Offer", menuName = "cOffer")]
 public class Offer : ScriptableObject
 {
+    public enum jobRole {
+        gamedesign, programming, art, production
+    }
+
     public string offerTitle;
     public string offerDescription;
+    public jobRole offerJobRole;
     public string companyTitle;
     public Sprite companyImage;
 
